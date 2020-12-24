d5a55246de9f6b52db26dc7e5479229a09f091f7
votrquan <votrquan@gmail.com>
2020-12-24 22:36:23 +0700

    Update js.js

--- Public/Develop/js.js
+++ Public/Develop/js.js
@@ -1,3 +1,18 @@
-function test(){
-		alert("hello there");
-	}
+function readURL(input) {
+  if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+          $('#blah')
+              .attr('src', e.target.result)
+              .width(200)
+              .height(200);
+      };
+
+      reader.readAsDataURL(input.files[0]);
+  }
+}
+
+function show(){
+  alert($('#blah').attr('src'));
+}
